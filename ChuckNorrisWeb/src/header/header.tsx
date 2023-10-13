7dfe4dbaedb05543c1479cc27793b4c3305b1203
Dominik <dominikreszczynski10@gmail.com>
2023-10-13 18:37:02 +0200

    finish header and burger

--- ChuckNorrisWeb/src/header/header.tsx
+++ ChuckNorrisWeb/src/header/header.tsx
@@ -2,9 +2,19 @@ import Logo from "../assets/hat.svg";
 import "./style.css";
 export const AppHeader = () => {
   return (
-    <div id="logo_conteiner">
-      <img id="logo" src={Logo} alt="Logo" />
-      <h3 id="title">CHUCK NORRIS WEB</h3>
-    </div>
+    <section className="top-nav">
+      <img className="logo" src={Logo} alt="" />
+      <input id="menu-toggle" type="checkbox" />
+      <label className="menu-button-container" for="menu-toggle">
+        <div className="menu-button"></div>
+      </label>
+      <ul className="menu">
+        <li>One</li>
+        <li>Two</li>
+        <li>Three</li>
+        <li>Four</li>
+        <li>Five</li>
+      </ul>
+    </section>
   );
 };
